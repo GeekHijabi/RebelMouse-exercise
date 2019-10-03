89989cff613b42cccf1e0e4d79e235fd17be9f6d
geekhijabi <bimpeadetunji@rocketmail.com>
2019-10-03 15:48:49 +0100

    setup content pages for different screen sizes

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener("click", function() {
+navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
+});
+
+$(document).ready(() => {
+  document.getElementById("#content-carousel").mouseover(() => {
+    document.getElementById("#content-carousel .carousel-control").show();
+  });
+
+  document.getElementById("#content-carousel").mouseleave(() => {
+    document.getElementById("#content-carousel .carousel-control").hide();
+  });
+
+  document.getElementById("#thumbnail-carousel .thumb").addEventListener("click", () => {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+
+  document.getElementById('#content-carousel').on('slid.bs.carousel', () => {
+    let index = document.getElementById('.carousel-inner .item.active').index();
+    let thumbnailActive = document.getElementById('#thumbnail-carousel .thumb[data-slide-to="' + index + '"]');
+    thumbnailActive.addClass('active');
+    $(thumbnailActive).siblings().removeClass("active");
+  });
 });
